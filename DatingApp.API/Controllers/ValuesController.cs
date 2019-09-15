2bb1e443b0cc7f1e27ff897769112db239c21bc1
eiskon <eiskon48@gmail.com>
2019-09-15 19:00:21 +0200

    AutController

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
-    // thhp://localhost:5000/api/values
+    //POST http://localhost:5000/api/values
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
